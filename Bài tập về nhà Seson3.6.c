1d122ea6a0666b3b381a1fb08e8686f77d57edfe
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2024-11-21 23:50:50 +0700

    Add files via upload

---
+++ Bài tập về nhà Seson3.6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("nhap canh:\n");
+    float canh,chieucao;
+    scanf("%f" "%f",&canh,&chieucao);
+    float s=canh*chieucao/2;
+    printf("dien tich tam giac la: %.2f\n",s);
+    printf("Hello world!\n");
+    return 0;
+}
